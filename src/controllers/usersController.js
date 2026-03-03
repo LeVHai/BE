71b1957e2faad98489bf82204a1bd39c49eebfb3
le hai <lhaigithub@gmail.com>
2026-03-03 22:56:04 +0700

    tạo access token và refresh token khi login

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,13 +1,25 @@
+import { HTTP_STATUS } from "../constants/httpStatus.js";
 import userService from "../services/userService.js";
 
 export const loginController = async (req, res) => {
   const { user } = req;
   const { _id } = user;
-  console.log(_id);
-  
-  const result = userService.login(_id);
+  const result = await userService.login(_id);
+  res.cookie("access_token", result.access_token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+    maxAge: 15 * 60 * 1000, // 15 phút
+  });
+  res.cookie("refresh_token", result.refresh_token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ngày
+  });
+
   res.json({
     message: "Đăng nhập thành công",
-    result,
+    status: HTTP_STATUS.OK,
   });
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,40 @@
 import { config } from "dotenv";
+import { TOKEN } from "../constants/type.js";
+import { signToken } from "../utils/jwt.js";
 config();
 class UserService {
- 
+  signAccessToken({ user_id }) {
+    return signToken({
+      payload: { user_id, token_type: TOKEN.ACCESS_TOKEN },
+      privateKey: process.env.JWT_SECRET_ACCESS_TOKEN,
+      options: {
+        algorithm: "HS256",
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+      },
+    });
+  }
+  signRefreshToken({ user_id }) {
+    return signToken({
+      payload: { user_id, token_type: TOKEN.REFRESH_TOKEN },
+      privateKey: process.env.JWT_SECRET_REFRESH_TOKEN,
+      options: {
+        algorithm: "HS256",
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+      },
+    });
+  }
+  signAccessTokenAndRefreshToken({ user_id }) {
+    return Promise.all([
+      this.signAccessToken({ user_id }),
+      this.signRefreshToken({ user_id }),
+    ]);
+  }
   async login({ user_id}) {
+    const [access_token, refresh_token] =
+      await this.signAccessTokenAndRefreshToken({
+        user_id:user_id,
+      });
+    return { access_token, refresh_token };
   }
   async logout(token) {}
 }
